b896007882774c4790fd5822e26bbefd2a6cf0ee
kirubeKinfe <kirubelkinfe01@gmail.com>
2023-11-21 18:15:15 +0300

    moving projects array to constants folder

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,28 +13,7 @@ export type ProjectType = {
 
 export const revalidate = 900;
 
-// const projects = [
-//     {
-//         _id: '1',
-//         title: "Kitfo Habesha",
-//         description: "A restaurant website that allows users to order food online with a payment Integration. The website provides an interface for selecting, comparing, and ordering foods.",
-//         projectLink: "https://kitfo-habesha.vercel.app",
-//         codeLink: "https://github.com/kirubelKinfe/RestaurantApp.git",
-//         imgUrl: "/assets/restaurant.png",
-//         tags: ["Next Js", "Typescript", "Sanity", "Stripe"]
 
-//     },
-//     {
-//         _id: '2',
-//         title: "E-Smart",
-//         description: "An online e-learning-platform that allows users to take courses online. The website also provides an interface for Instructors to create and publish their courses.",
-//         projectLink: "https://e-esmart.netlify.app",
-//         codeLink: "https://github.com/kirubelKinfe/E-learning-Platform.git",
-//         imgUrl: "/assets/e-learning.png",
-//         tags: ["React Js", "Typescript", "Node Js", "Express Js", "MongoDB"]
-
-//     },
-// ]
 
 const Projects = async () => {
     const projects = await getProjects({ query: "", category: "", page: "1" })
